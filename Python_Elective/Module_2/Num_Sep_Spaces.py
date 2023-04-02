c326002067265bdc66e2686df630be2b0ddc2bb3
ClementMathew <clementmathew924@gmail.com>
2023-04-02 12:35:01 +0530

    New

--- Python_Elective/Module_2/Num_Sep_Spaces.py
+++ Python_Elective/Module_2/Num_Sep_Spaces.py
@@ -1,6 +1,10 @@
-f= open("integersline","r")
+f= open("integersline.txt","r")
 theSum=0
 
 for line in f:
     wordlist = line.split()
-    
+    for word in wordlist:
+        number = int(word)
+        theSum += number
+
+print("The sum is",theSum)
